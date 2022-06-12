204a8610306b9fa7e5b97b5c4561a4ad8ab90567
Konstantin Lemko <lemko99@bk.ru>
2022-06-12 15:18:49 +0700

    fix: add check on directory to cd operation

--- src/operations/cd.js
+++ src/operations/cd.js
@@ -1,10 +1,17 @@
+import { stat } from 'fs/promises'
 import { logError } from '../utils/logError.js';
 import { getPath } from '../utils/getPath.js';
 
 export async function cd(currentPath, inputedPath) {
   try {
-    return await getPath(currentPath, inputedPath);
+    const pathToFolder = await getPath(currentPath, inputedPath);
+    const fileStat = await stat(pathToFolder);
+    if (fileStat.isFile()) {
+      throw new Error();
+    }
+    return pathToFolder;
   } catch {
     logError();
   }
+  return currentPath;
 }
