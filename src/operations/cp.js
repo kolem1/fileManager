78281d9b683afe1d5a063e51cede236cdd7bfb6a
Konstantin Lemko <lemko99@bk.ru>
2022-06-13 01:45:21 +0700

    refactor: copy file with streams

--- src/operations/cp.js
+++ src/operations/cp.js
@@ -1,5 +1,4 @@
-import { copyFile } from 'fs/promises';
-import { constants } from 'fs';
+import { createReadStream, createWriteStream } from 'fs';
 import { basename, join } from 'path';
 import { getFilePath, getPath, logError } from "../utils/index.js";
 
@@ -8,7 +7,9 @@ export async function cp(currentPath, userPathToFile, pathToNewDirectory) {
     const pathToFile = await getFilePath(currentPath, userPathToFile, pathToNewDirectory);
     const fileName = basename(pathToFile);
     const newPath = await getPath(currentPath, join(pathToNewDirectory, fileName));
-    await copyFile(pathToFile, newPath, constants.COPYFILE_EXCL);
+    const readStream = createReadStream(pathToFile);
+    const writeStream = createWriteStream(newPath, { flags: 'wx' });
+    readStream.pipe(writeStream);
   } catch {
     logError();
   }
