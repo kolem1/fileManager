f4143da31e0d33028766199e605df2b47f16eeee
Konstantin Lemko <lemko99@bk.ru>
2022-06-12 15:54:27 +0700

    feat: add add operation

--- src/checkInput.js
+++ src/checkInput.js
@@ -3,6 +3,7 @@ import { up } from './operations/up.js';
 import { cd } from './operations/cd.js';
 import { ls } from './operations/ls.js';
 import { cat } from './operations/cat.js';
+import { add } from './operations/add.js';
 
 export async function checkInput(input, currentPath) {
   const [operation, ...args] = input.split(' ');
@@ -15,6 +16,9 @@ export async function checkInput(input, currentPath) {
       return await ls(currentPath);
     case 'cat':
       await cat(currentPath, args[0]);
+      break;
+    case 'add':
+      await add(currentPath, args[0]);
       break;
     default:
       console.log('Invalid input')
