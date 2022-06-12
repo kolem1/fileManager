29b8759f9815615b197ee80feef719ae95876f60
Konstantin Lemko <lemko99@bk.ru>
2022-06-13 02:43:53 +0700

    fix: fix name request

--- src/main.js
+++ src/main.js
@@ -5,18 +5,18 @@ import readline from 'readline';
 const args = process.argv.slice(2);
 
 async function run() {
-  let nameArg = args.find((arg) => arg.startsWith('--username'));
-  while (!nameArg) {
+  const nameArg = args.find((arg) => arg.startsWith('--username'));
+  let name = nameArg?.split('=')[1];
+  while (!name) {
     console.log('Please, input your name');
     await new Promise((resolve) => {
       process.stdin.on('data', (data) => {
-        nameArg = data.toString();
+        name = data.toString().trim();
         process.stdin.end();
         resolve();
       })
     })
   }
-  const name = nameArg.split('=')[1];
   console.log(`\nWelcome to the File Manager, ${name}!`);
 
   const rl = readline.createInterface({
